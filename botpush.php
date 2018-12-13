07ec139d0c6ca583a1919b3a16317790d1076050
amphon072 <36217551+amphon072@users.noreply.github.com>
2018-12-13 17:32:25 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -1,21 +1,22 @@
 <?php
-
-
-
 require "vendor/autoload.php";
 
 $access_token = 'nxaCamU8iNqJ6IbQE2cXR8NWjXNWUQmtJy1HB9wb4aDWYa7Xxm/+uWAt2JNXr+Skxv4fYl/sSExsHAksnbxCChEKHdx2sZgFrB1thQbTUfZfGvBYrY2rM44CkHuhaESvKIstQEw1eb1BGjAMIjNsewdB04t89/1O/w1cDnyilFU=';
 
 $channelSecret = '6c8a10d0968592481b9e15fe67decaeb';
 
-$pushID = '79bc269f1c99431eae9eac7d7e54ac08';
+$pushID = '95910062aef546ed8fec6432c34670d5';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
-$response = $bot->pushMessage($idPush, $textMessageBuilder);
+
+$response = $bot->pushMessage($pushID, $textMessageBuilder);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+
 
 
 
