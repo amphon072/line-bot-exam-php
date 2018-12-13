91439ff3fc95f28c95d7a4c0904884f7ce3d7675
amphon072 <36217551+amphon072@users.noreply.github.com>
2018-12-13 16:57:49 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -8,7 +8,7 @@ $access_token = 'nxaCamU8iNqJ6IbQE2cXR8NWjXNWUQmtJy1HB9wb4aDWYa7Xxm/+uWAt2JNXr+S
 
 $channelSecret = '6c8a10d0968592481b9e15fe67decaeb';
 
-$pushID = '58b19fc71890';
+$pushID = '54d6b6782-b7be-45cb-973c-b05fde9725cd';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
