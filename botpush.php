5e60b88321037789da4403a28603241163edc77d
amphon072 <36217551+amphon072@users.noreply.github.com>
2018-12-16 14:44:07 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -1,24 +1,34 @@
+
+
 <?php
-require "vendor/autoload.php";
-
-$access_token = 'nxaCamU8iNqJ6IbQE2cXR8NWjXNWUQmtJy1HB9wb4aDWYa7Xxm/+uWAt2JNXr+Skxv4fYl/sSExsHAksnbxCChEKHdx2sZgFrB1thQbTUfZfGvBYrY2rM44CkHuhaESvKIstQEw1eb1BGjAMIjNsewdB04t89/1O/w1cDnyilFU=';
-
-$channelSecret = '6c8a10d0968592481b9e15fe67decaeb';
-
-$pushID = '95910062aef546ed8fec6432c34670d5';
-
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
-
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
-
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
-
-$response = $bot->pushMessage($pushID, $textMessageBuilder);
-
-echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
-
-
-
+ 
+$strAccessToken = "q766aNO1Wf6wOS4w0DB2XaF1rq/IlsDqsUGgTvTQwcHmj8GV3ffFAWkr4L5E5qFyrsVrPlW6ciTVN9rusIDHLzPTR5UIITjKxRxPz22Da+wMlqQAti0KoL4L9t4yg/y2QoADmOe9S2Q31OVT6xjiBgdB04t89/1O/w1cDnyilFU=
+";
+ 
+$strUrl = "https://api.line.me/v2/bot/message/push";
+ 
+$arrHeader = array();
+$arrHeader[] = "Content-Type: application/json";
+$arrHeader[] = "Authorization: Bearer {$strAccessToken}";
+ 
+$arrPostData = array();
+$arrPostData['to'] = "USER_ID";
+$arrPostData['messages'][0]['type'] = "text";
+$arrPostData['messages'][0]['text'] = "นี้คือการทดสอบ Push Message";
+ 
+ 
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,$strUrl);
+curl_setopt($ch, CURLOPT_HEADER, false);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+$result = curl_exec($ch);
+curl_close ($ch);
+ 
+?>
 
 
 
